2bc16cbd8e95a00d7f1e3ca08cbc92fb896e18b9
ydipeepo <ydipeepohappy@outlook.com>
2021-02-19 10:59:15 +0900

    added null logger impl

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,30 @@ import ConsoleLoggerFactory from "./ConsoleLoggerFactory";
 import Logger from "./Logger";
 import LoggerConfiguration from "./LoggerConfiguration";
 import LoggerFactory from "./LoggerFactory";
+import NullLoggerFactory from "./NullLoggerFactory";
 import Severity from "./Severity";
 
-const defaultLoggerFactory: LoggerFactory = new ConsoleLoggerFactory();
+let loggerFactory: LoggerFactory | undefined;
+
+function getLoggerFactory() {
+	loggerFactory ??= new ConsoleLoggerFactory();
+	return loggerFactory;
+}
 
 function createLogger(name: string) {
-	return defaultLoggerFactory.create(name);
+	return getLoggerFactory().create(name);
 }
 
 function suppress(...names: string[]): void {
-	defaultLoggerFactory.suppress(...names);
+	getLoggerFactory().suppress(...names);
 }
 
 export {
 	createLogger,
 	suppress,
+	Severity,
 	Logger,
 	LoggerConfiguration,
 	LoggerFactory,
-	Severity,
+	NullLoggerFactory,
 }
