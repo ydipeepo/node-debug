965e241398fb85371279257d7a4cf14f486c70f0
ydipeepo <ydipeepohappy@outlook.com>
2021-02-23 18:32:28 +0900

    removed some that was causing package dep problem

--- src/ConsoleLoggerFactory.ts
+++ src/ConsoleLoggerFactory.ts
@@ -4,22 +4,29 @@ import Logger from "./Logger";
 import LoggerFactory from "./LoggerFactory";
 import Severity from "./Severity";
 
+/**
+ * コンソールへ出力する Logger インスタンスを作成するために使用する型を表します。
+ */
 export default class ConsoleLoggerFactory implements LoggerFactory {
 
 	private readonly configuration: ConsoleLoggerConfiguration = new ConsoleLoggerConfiguration();
 
+	/** @inheritdoc */
 	get severity() {
 		return this.configuration.severity;
 	}
 
+	/** @inheritdoc */
 	set severity(value: Severity) {
 		this.configuration.severity = value;
 	}
 
+	/** @inheritdoc */
 	create(name: string): Logger {
 		return new ConsoleLogger(name, this.configuration);
 	}
 
+	/** @inheritdoc */
 	suppress(...names: string[]) {
 		this.configuration.suppress(names);
 	}
